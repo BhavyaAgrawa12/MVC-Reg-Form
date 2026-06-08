4017f3bcb0f7a19312787fd15f2dcfa5cebdab64
BhavyaAgrawa12git config --global user.name BhavyaAgrawa12 <agrawalbhavya943@gmail.com>
2026-06-08 12:49:16 +0530

    Move MongoDB credentials to env

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-
-const uri = 'mongodb://agrawalbhavya:admin@ac-8ydfdox-shard-00-00.3orx8wx.mongodb.net:27017,ac-8ydfdox-shard-00-01.3orx8wx.mongodb.net:27017,ac-8ydfdox-shard-00-02.3orx8wx.mongodb.net:27017/?ssl=true&replicaSet=atlas-j4fqti-shard-0&authSource=admin&appName=mvc-form';
 
 async function connectDB() {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
